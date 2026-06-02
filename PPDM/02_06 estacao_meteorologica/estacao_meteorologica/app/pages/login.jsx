ce19a9b499cbf0f591d0cf86343605c034245b85
Luckydfy <fernandayaoita@gmail.com>
2026-06-02 11:01:43 -0300

    02/06

---
+++ PPDM/02_06 estacao_meteorologica/estacao_meteorologica/app/pages/login.jsx
@@ -0,0 +1,7 @@
+import {View, Text} from 'react-native';
+
+export default function Login({navigation}) {
+    <View>
+        <Text>Página de Login</Text>
+    </View>
+}
---
+++ PPDM/02_06 estacao_meteorologica/estacao_meteorologica/app/pages/registro.jsx
@@ -0,0 +1,7 @@
+import {View, Text} from 'react-native';
+
+export default function Registro({navigation}) {
+    <View>
+        <Text>Página de Registro</Text>
+    </View>
+}
